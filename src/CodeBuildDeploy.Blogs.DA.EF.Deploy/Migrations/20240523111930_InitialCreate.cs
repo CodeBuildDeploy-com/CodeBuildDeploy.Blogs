d42d8e8747ceb38e10541aa97885652e3e056e58
Mark Pollard <mark.pollard@bjss.co.uk>
2024-05-23 15:44:52 +0100

    CBD-28: Automating service schema deployment

---
+++ src/CodeBuildDeploy.Blogs.DA.EF.Deploy/Migrations/20240523111930_InitialCreate.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeBuildDeploy.Blogs.DA.EF.Deploy.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "blg");
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                schema: "blg",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                schema: "blg",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                schema: "blg",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UrlSlug = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Published = table.Column<bool>(type: "bit", nullable: false),
+                    PostedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Category_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Post_Category_Category_Id",
+                        column: x => x.Category_Id,
+                        principalSchema: "blg",
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostTag",
+                schema: "blg",
+                columns: table => new
+                {
+                    Post_Id = table.Column<int>(type: "int", nullable: false),
+                    Tag_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostTag", x => new { x.Post_Id, x.Tag_Id });
+                    table.ForeignKey(
+                        name: "FK_PostTag_Post_Post_Id",
+                        column: x => x.Post_Id,
+                        principalSchema: "blg",
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PostTag_Tag_Tag_Id",
+                        column: x => x.Tag_Id,
+                        principalSchema: "blg",
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_Category_Id",
+                schema: "blg",
+                table: "Post",
+                column: "Category_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostTag_Tag_Id",
+                schema: "blg",
+                table: "PostTag",
+                column: "Tag_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostTag",
+                schema: "blg");
+
+            migrationBuilder.DropTable(
+                name: "Post",
+                schema: "blg");
+
+            migrationBuilder.DropTable(
+                name: "Tag",
+                schema: "blg");
+
+            migrationBuilder.DropTable(
+                name: "Category",
+                schema: "blg");
+        }
+    }
+}
---
+++ src/CodeBuildDeploy.Blogs.DA.EF/DI/ServicesRegistration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using CodeBuildDeploy.Blogs.DA.EF.Queries;
+using CodeBuildDeploy.Blogs.DA.Entities;
+using CodeBuildDeploy.Blogs.DA.Queries;
+
+namespace CodeBuildDeploy.Blogs.DA.EF.DI
+{
+    public static class ServicesRegistration
+    {
+        private const string EFMigrationsHistoryTableName = "__EFMigrationsHistory";
+
+        public static IServiceCollection ConfigureDataServices(this IServiceCollection services)
+        {
+            services.AddDbContext<DAContext>((serviceProvider, options) =>
+                options.UseSqlServer(serviceProvider.GetService<IConfiguration>()!.GetConnectionString("BlogConnection"),
+                                     x => x.MigrationsHistoryTable(EFMigrationsHistoryTableName, DAContext.SchemaName)));
+
+            services.ConfigureQueryRunners();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureDataServices(this IServiceCollection services, string migrationsAssemblyName)
+        {
+            services.AddDbContext<DAContext>((serviceProvider, options) =>
+                options.UseSqlServer(serviceProvider.GetService<IConfiguration>()!.GetConnectionString("BlogMigrationConnection"),
+                                     x => x.MigrationsHistoryTable(EFMigrationsHistoryTableName, DAContext.SchemaName)
+                                           .MigrationsAssembly(migrationsAssemblyName)));
+
+            services.ConfigureQueryRunners();
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureQueryRunners(this IServiceCollection services)
+        {
+            services.AddScoped<IQueryRunner<AllCategoriesQuery, IList<Category>>, AllCategoriesQueryRunner>();
+            services.AddScoped<IQueryRunner<AllPostsQuery, IList<Post>>, AllPostsQueryRunner>();
+            services.AddScoped<IQueryRunner<PagedPostsQuery, IList<Post>>, PagedPostsQueryRunner>();
+            services.AddScoped<IQueryRunner<PostByUrlSlugQuery, Post>, PostByUrlSlugQueryRunner>();
+            services.AddScoped<IQueryRunner<TotalPostsQuery, int>, TotalPostsQueryRunner>();
+
+            return services;
+        }
+    }
+}
